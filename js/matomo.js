d99fc0999f09310a1dffb50fd08ae938a1dff85a
Daniel Baudry <dbaudry@octo.com>
2019-07-12 15:38:49 +0200

    (PC-2100) Add matomo to homepage

---
+++ js/matomo.js
@@ -0,0 +1,11 @@
+var _paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://pc-matomo-production.scalingo.io/";
+  var siteId=1;
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', siteId]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
